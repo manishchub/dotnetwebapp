423c540c27df15e874ee4ddf37d832e8789bd8d3
manishchub <tabnexus12@gmail.com>
2025-02-15 00:45:32 +0530

    Update Program.cs

--- src/DotNetWebApp/Program.cs
+++ src/DotNetWebApp/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// Listen on all interfaces (important for containerized apps)
+app.Urls.Add("http://0.0.0.0:80");
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
